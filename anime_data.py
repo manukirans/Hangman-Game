d34d04076c6e7f001aa4c0346adc80e27b374e44
Manu Kiran S <143595414+manukirans@users.noreply.github.com>
2024-06-23 18:13:58 +0530

    Add files via upload

---
+++ anime_data.py
@@ -0,0 +1,87 @@
+# List of anime characters
+anime_characters = [
+    'Luffy', 'Zoro', 'Nami', 'Sanji', 'Usopp', 'Chopper', 'Robin', 'Franky', 'Brook', 'Jinbe',  # One Piece
+    'Ichigo', 'Rukia', 'Renji', 'Orihime', 'Uryu', 'Byakuya', 'Kenpachi', 'Toshiro', 'Gin', 'Aizen',  # Bleach
+    'Gojo', 'Yuji', 'Megumi', 'Nobara', 'Sukuna', 'Maki', 'Panda', 'Yuta', 'Kugisaki', 'Inumaki',  # Jujutsu Kaisen
+    'Goku', 'Vegeta', 'Gohan', 'Piccolo', 'Frieza', 'Cell', 'MajinBuu', 'Trunks', 'Krillin', 'Bulma',  # Dragon Ball Z
+    'Tanjiro', 'Nezuko', 'Zenitsu', 'Inosuke', 'Giyu', 'Shinobu', 'Kanao', 'Tengen', 'Muzan', 'Rengoku',  # Demon Slayer
+    'Asta', 'Yuno', 'Noelle', 'Yami', 'Magna', 'Luck', 'Fuegoleon', 'Vetto', 'Vanessa', 'William',  # Black Clover
+    'SungJinWoo', 'ChaHaeIn', 'Igris', 'Beru', 'YooJinHo', 'GoGunHee', 'BaeYoonSik', 'LeeJuHee', 'SongJiHoon', 'KangTaeShik'  # Solo Leveling
+    
+    # add more 
+]
+
+clue_anime_series = {
+    'Luffy': 'One Piece',
+    'Zoro': 'One Piece',
+    'Nami': 'One Piece',
+    'Sanji': 'One Piece',
+    'Usopp': 'One Piece',
+    'Chopper': 'One Piece',
+    'Robin': 'One Piece',
+    'Franky': 'One Piece',
+    'Brook': 'One Piece',
+    'Jinbe': 'One Piece',
+    'Ichigo': 'Bleach',
+    'Rukia': 'Bleach',
+    'Renji': 'Bleach',
+    'Orihime': 'Bleach',
+    'Uryu': 'Bleach',
+    'Byakuya': 'Bleach',
+    'Kenpachi': 'Bleach',
+    'Toshiro': 'Bleach',
+    'Gin': 'Bleach',
+    'Aizen': 'Bleach',
+    'Gojo': 'Jujutsu Kaisen',
+    'Yuji': 'Jujutsu Kaisen',
+    'Megumi': 'Jujutsu Kaisen',
+    'Nobara': 'Jujutsu Kaisen',
+    'Sukuna': 'Jujutsu Kaisen',
+    'Maki': 'Jujutsu Kaisen',
+    'Panda': 'Jujutsu Kaisen',
+    'Yuta': 'Jujutsu Kaisen',
+    'Kugisaki': 'Jujutsu Kaisen',
+    'Inumaki': 'Jujutsu Kaisen',
+    'Goku': 'Dragon Ball Z',
+    'Vegeta': 'Dragon Ball Z',
+    'Gohan': 'Dragon Ball Z',
+    'Piccolo': 'Dragon Ball Z',
+    'Frieza': 'Dragon Ball Z',
+    'Cell': 'Dragon Ball Z',
+    'MajinBuu': 'Dragon Ball Z',
+    'Trunks': 'Dragon Ball Z',
+    'Krillin': 'Dragon Ball Z',
+    'Bulma': 'Dragon Ball Z',
+    'Tanjiro': 'Demon Slayer',
+    'Nezuko': 'Demon Slayer',
+    'Zenitsu': 'Demon Slayer',
+    'Inosuke': 'Demon Slayer',
+    'Giyu': 'Demon Slayer',
+    'Shinobu': 'Demon Slayer',
+    'Kanao': 'Demon Slayer',
+    'Tengen': 'Demon Slayer',
+    'Muzan': 'Demon Slayer',
+    'Rengoku': 'Demon Slayer',
+    'Asta': 'Black Clover',
+    'Yuno': 'Black Clover',
+    'Noelle': 'Black Clover',
+    'Yami': 'Black Clover',
+    'Magna': 'Black Clover',
+    'Luck': 'Black Clover',
+    'Fuegoleon': 'Black Clover',
+    'Vetto': 'Black Clover',
+    'Vanessa': 'Black Clover',
+    'William': 'Black Clover',
+    'SungJinWoo': 'Solo Leveling',
+    'ChaHaeIn': 'Solo Leveling',
+    'Igris': 'Solo Leveling',
+    'Beru': 'Solo Leveling',
+    'YooJinHo': 'Solo Leveling',
+    'GoGunHee': 'Solo Leveling',
+    'BaeYoonSik': 'Solo Leveling',
+    'LeeJuHee': 'Solo Leveling',
+    'SongJiHoon': 'Solo Leveling',
+    'KangTaeShik': 'Solo Leveling'
+    
+    # add more
+    }
